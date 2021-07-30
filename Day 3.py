2ed0f1b71c6ea24d7e9c0d85898b571c895a74dd
sunilghanchi <88071734+sunilghanchi@users.noreply.github.com>
2021-07-30 13:03:22 +0530

    Day 3 - Assignment - Python Zero to Hero

---
+++ Day 3.py
@@ -0,0 +1,11 @@
+#Day3
+#Question - 1 
+#Consider a person is represented by Pi, where i is the index of the following list.
+#A list shows the person to whom person Pi has given the gift.
+
+gift_represented_to = [5, 3, 4, 2, 1]
+
+#so for the given list, the list of persons from whom they have received the gift would be
+
+gift_received_from = [5, 4, 2, 3, 1]
+print("Gift Received from: ", gift_received_from)
